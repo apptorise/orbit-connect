ce530f77e67d766696879b46184841f4c9a3d71a
Leroy Ramaphoko <adventapp777@gmail.com>
2026-02-02 16:17:38 +0200

    Refresh token

--- lib/grpc/src/main/java/com/apptorise/orbit/connect/grpc/OrbitHeaderInterceptor.kt
+++ lib/grpc/src/main/java/com/apptorise/orbit/connect/grpc/OrbitHeaderInterceptor.kt
@@ -1,18 +1,10 @@
 package com.apptorise.orbit.connect.grpc
 
 import io.grpc.*
-import android.util.Base64
 
 class OrbitHeaderInterceptor(
-    private val userAgent: String,
-    private val deviceInfoJson: String? = null
+    private val headersMap: Map<String, String> = emptyMap()
 ) : ClientInterceptor {
-
-    companion object {
-        private val USER_AGENT_KEY: Metadata.Key<String> = Metadata.Key.of("user-agent", Metadata.ASCII_STRING_MARSHALLER)
-
-        private val DEVICE_INFO_KEY: Metadata.Key<String> = Metadata.Key.of("x-device-info", Metadata.ASCII_STRING_MARSHALLER)
-    }
 
     override fun <ReqT, RespT> interceptCall(
         method: MethodDescriptor<ReqT, RespT>,
@@ -23,11 +15,10 @@ class OrbitHeaderInterceptor(
             next.newCall(method, callOptions)
         ) {
             override fun start(listener: Listener<RespT>, headers: Metadata) {
-                headers.put(USER_AGENT_KEY, userAgent)
-
-                deviceInfoJson?.let { json ->
-                    val encodedInfo = Base64.encodeToString(json.toByteArray(), Base64.NO_WRAP)
-                    headers.put(DEVICE_INFO_KEY, encodedInfo)
+                headersMap.forEach { (key, value) ->
+                    val metadataKey = Metadata.Key.of(key, Metadata.ASCII_STRING_MARSHALLER)
+                    headers.removeAll(metadataKey)
+                    headers.put(metadataKey, value)
                 }
 
                 super.start(listener, headers)
