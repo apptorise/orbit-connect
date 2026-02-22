249e4822d94f60aba61b1105a7fcf39ffab671d4
Leroy Ramaphoko <adventapp777@gmail.com>
2026-02-23 00:41:24 +0200

    Pass mock file

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ subprojects {
                     create<MavenPublication>("release") {
                         groupId = "com.github.apptorise"
                         artifactId = "orbit-connect-${project.name}"
-                        version = "1.0.32"
+                        version = "1.0.31"
 
                         afterEvaluate {
                             from(components["release"])
